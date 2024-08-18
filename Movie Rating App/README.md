ee1c5f2c2f08458a54bd5c8c2037f7ec8fddfe11
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2024-08-18 20:45:38 +0530

    New Movie Rating App Basic Files Generated

--- Movie Rating App/README.md
+++ Movie Rating App/README.md
@@ -1,4 +1,4 @@
-# UserManagement
+# MovieRatingApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.8.
 
@@ -12,7 +12,7 @@ Run `ng generate component component-name` to generate a new component. You can 
 
 ## Build
 
-Run `ng serve` to build the project. The build artifacts will be stored in the `dist/` directory.
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
 
 ## Running unit tests
 
--- Movie Rating App/src/app/app.component.spec.ts
+++ Movie Rating App/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'User_Management'`, () => {
+  it(`should have as title 'Movie_Rating_App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('User_Management');
+    expect(app.title).toEqual('Movie_Rating_App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, User_Management');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Movie_Rating_App');
   });
 });
