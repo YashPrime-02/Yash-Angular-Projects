ecf9212b2a42cb235004f99ba5f0933df5f2f06c
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2024-08-19 16:05:27 +0530

    added both forms

--- BudgetPlanner_App/src/app/app.module.ts
+++ BudgetPlanner_App/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    CommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- BudgetPlanner_App/src/app/login/login.component.ts
+++ BudgetPlanner_App/src/app/login/login.component.ts
@@ -1,10 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
+  standalone:true, //must required for imports
+
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: any;
+  registerForm: any;
+activeForm: 'login' | 'register'='login'; //to check which form is active rn by default its login one
+//rest toggle is achieved at html part
+constructor(private fb: FormBuilder){}
 
+
+ngOnInit()
+{
+
+this.loginForm=this.fb.group({
+ email: ['', [Validators.required, Validators.email]],
+ password: ['', Validators.required]
+});
+
+this.registerForm=this.fb.group({
+  username: ['', Validators.required],
+  email: ['', [Validators.required, Validators.email]],
+  password: ['', Validators.required]
+ });
+
+
+
+
+
+}
 }
