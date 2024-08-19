d29b4e041d7ce5dad56af1673b873327a3e4c32f
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2024-08-19 17:25:50 +0530

    Login Component Done

--- BudgetPlanner_App/src/app/app.module.ts
+++ BudgetPlanner_App/src/app/app.module.ts
@@ -1,23 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @NgModule({
+
   declarations: [
     AppComponent,
-    LoginComponent,
+
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     ReactiveFormsModule,
     CommonModule,
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
